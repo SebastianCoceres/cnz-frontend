d71066e799b7adadc7fd1b1ad1c525d176d8ec39
Sebastian Coceres <sebastian.coceres11@gmail.com>
2022-10-25 22:14:29 +0200

    teamcard fix

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -9,7 +9,7 @@ import {
 function TeamCard({ person, job, social }) {
   return (
     <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-      <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <div className="h-full flex items-start border-gray-200 border p-4 rounded-lg">
         <div className="flex-grow">
           <h2 className="text-gray-900 title-font font-medium">{person}</h2>
           <p className="text-gray-500">{job}</p>
